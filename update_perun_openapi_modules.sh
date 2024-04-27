97e401cddafc9f0353fb5fd76539ad7b48823b8e
Šimon Brauner <simonbrauner@seznam.cz>
2024-04-27 16:04:33 +0200

    feat: update api script

---
+++ update_perun_openapi_modules.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+IMPORT_PREFIX="ansible_collections.simonbrauner.perun.plugins.module_utils."
+
+PERUN_SOURCE="https://gitlab.ics.muni.cz/perun/perun-idm/perun.git"
+PERUN_OPENAPI_DIR_NAME="perun_openapi"
+PERUN_OPENAPI_README_NAME="perun_openapi_README.md"
+
+PERUN_OPENAPI_DIR="$(pwd)/plugins/module_utils/$PERUN_OPENAPI_DIR_NAME"
+TMP_DIR="$(mktemp -d /tmp/perun_openapi.XXXXXX)"
+
+rm -rf "$PERUN_OPENAPI_DIR"
+
+git clone --depth 1 "$PERUN_SOURCE" "$TMP_DIR"
+cd "$TMP_DIR/perun-cli-python"
+
+sh "generate.sh"
+
+MATCH="^(from |import )$PERUN_OPENAPI_DIR_NAME"
+find "$PERUN_OPENAPI_DIR_NAME" -name "*.py" -exec sed -E -i \
+    "s/$MATCH/\1$IMPORT_PREFIX$PERUN_OPENAPI_DIR_NAME/" {} +
+
+cp -r "$PERUN_OPENAPI_DIR_NAME" "$PERUN_OPENAPI_DIR"
+cp "$PERUN_OPENAPI_README_NAME" "$PERUN_OPENAPI_DIR/README.md"
+
+rm -rf "$TMP_DIR"
